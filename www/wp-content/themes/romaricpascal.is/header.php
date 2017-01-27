a54126d45582b7d86c4b1bfe087e2947bc47f9d6
Romaric Pascal <romaric.pascal@gmail.com>
2017-01-27 17:56:38 +0000

    Added favicons

--- www/wp-content/themes/romaricpascal.is/header.php
+++ www/wp-content/themes/romaricpascal.is/header.php
@@ -12,6 +12,17 @@
       <?php if(wp_title(' ', false)) { echo '|'; } ?>
       <?php bloginfo('name'); ?>
     </title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_theme_file_uri('assets/images/favicons/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" href="<?php echo get_theme_file_uri('assets/images/favicons/favicon-32x32.png') ?>" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo get_theme_file_uri('assets/images/favicons/android-chrome-192x192.png') ?>" sizes="192x192">
+    <link rel="icon" type="image/png" href="<?php echo get_theme_file_uri('assets/images/favicons/favicon-16x16.png') ?>" sizes="16x16">
+    <link rel="manifest" href="<?php echo get_theme_file_uri('assets/images/favicons/manifest.json') ?>">
+    <link rel="mask-icon" href="<?php echo get_theme_file_uri('assets/images/favicons/safari-pinned-tab.svg') ?>" color="#260b23">
+    <meta name="msapplication-TileColor" content="#260b23">
+    <meta name="msapplication-TileImage" content="<?php echo get_theme_file_uri('assets/images/favicons/mstile-144x144.png') ?>">
+    <meta name="theme-color" content="#260b23">
+
     <?php wp_head();?>
   </head>
   <body>
