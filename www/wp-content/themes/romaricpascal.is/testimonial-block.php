d25247d9e921b9dbf0ba23afba96ec38f804b62d
rhumaric <romaric.pascal@gmail.com>
2017-01-31 11:25:28 +0000

    Styled the testimonial section

--- www/wp-content/themes/romaricpascal.is/testimonial-block.php
+++ www/wp-content/themes/romaricpascal.is/testimonial-block.php
@@ -1,5 +1,5 @@
 <?php global $post; ?>
-<figure>
-  <div><?php the_content(); ?></div>
-  <figcaption><?php the_testimonial_author() ?><?php the_testimonial_link(', ') ?></figcaption>
+<figure class="rp-Testimonial">
+  <div class="rp-TestimonialContent"><?php the_content(); ?></div>
+  <figcaption class="rp-TestimonialSource"><?php the_testimonial_author() ?><?php the_testimonial_link(', ') ?></figcaption>
 </figure>
