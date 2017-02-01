bb6869fc943a5f17145318f76f4c6f8fb34a63f0
rhumaric <romaric.pascal@gmail.com>
2017-02-01 11:41:45 +0000

    Fixed styling of project single page

--- www/wp-content/themes/romaricpascal.is/single-project.php
+++ www/wp-content/themes/romaricpascal.is/single-project.php
@@ -3,13 +3,15 @@
   the_post();
   $craft = get_craft($post); ?>
 <?php get_header(); ?>
-<main class="rp-ProjectFull rp-LandingSection">
+<main class="rp-ProjectFull">
   <div class="l-Container">
-    <header>
-      <div class="rp-ProjectFullThumbnail"><?php the_post_thumbnail(); ?></div>
-      <h1 class="rp-ProjectFullHeading rp-SectionTitle rp-Underlined"><?php the_title(); ?></h1>
+    <div class="rp-ProjectFullThumbnail"><?php the_post_thumbnail(); ?></div>
+    <header class="rp-ProjectFullHeader">
+        <h1 class="rp-ProjectFullHeading rp-SectionTitle rp-Underlined"><?php the_title(); ?></h1>
     </header>
-    <?php the_content(); ?>
+    <div class="rp-ProjectFullBody">
+      <?php the_content(); ?>
+    </div>
     <footer class="rp-ProjectFullFooter">
       <span class="rp-ProjectFullFooter__previous"><?php previous_post_link(); ?></span>
       <span class="rp-ProjectFullFooter__next"><?php next_post_link(); ?></span>
