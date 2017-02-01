54155148dca96810cebcdb8d34ac84605383d02d
rhumaric <romaric.pascal@gmail.com>
2017-02-01 17:41:40 +0000

    Added basic 404 page

---
+++ www/wp-content/themes/romaricpascal.is/404.php
@@ -0,0 +1,8 @@
+<?php get_header();?>
+<section class="rp-LandingSection">
+  <div class="l-Container">
+    <h1 class="rp-SectionTitle rp-Underlined">404</h1>
+    <p>Aarrgh :( Looks like there's nothing at this address.</p>
+  </div>
+</section>
+<?php get_footer();?>
