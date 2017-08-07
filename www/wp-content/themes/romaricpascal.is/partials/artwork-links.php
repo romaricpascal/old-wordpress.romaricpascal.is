a3efb8711e3a07b095b1f5688e35eb8b778c1665
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-07 18:45:45 +0100

    Made the shop link open in a new window

--- www/wp-content/themes/romaricpascal.is/partials/artwork-links.php
+++ www/wp-content/themes/romaricpascal.is/partials/artwork-links.php
@@ -4,7 +4,7 @@ $relatedProject = get_field(ARTWORK_RELATED_PROJECT_FIELD);
 if (!empty($shopURL) || !empty($relatedProject)): ?>
 <section class="rp-ArtworkSingleSection">
   <?php if(!empty($shopURL)): ?>
-    <a class="rp-ArtworkSingleLink" href="<?php echo $shopURL; ?>">Buy it in the shop.</a>
+    <a class="rp-ArtworkSingleLink" href="<?php echo $shopURL; ?>" target="_blank">Buy it in the shop.</a>
   <?php endif; ?>
   <?php if(!empty($relatedProject)): ?>
     <a class="rp-ArtworkSingleLink" href="<?php echo get_permalink($relatedProject[0]); ?>">Check out the whole project.</a>
