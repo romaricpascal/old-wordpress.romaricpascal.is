79cbd23e59e7bf948f160234b9c9e0cf5f6841b3
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-08 22:53:34 +0100

    Added two column layout for artwork archive page when space is sufficient

--- www/wp-content/themes/romaricpascal.is/archive-artwork.php
+++ www/wp-content/themes/romaricpascal.is/archive-artwork.php
@@ -3,14 +3,14 @@
 <?php if($with_layout): ?>
 <?php get_header(); ?>
 <article class="rp-ArtworkArchive">
-  <header>
+  <header class="rp-ArtworkArchive__header">
     <h1>Artworks</h1>
     <p>If you’re keen on seing behind the scene shots, work in progress, this is the place. I post regular pictures of what I’m working on, from sketches to finished artworks.</p>
     <p>Add the page to your RSS reader or follow me on Twitter/Instagram to make sure you don’t miss anything.</p>
   </header>
 <?php endif; ?>
 
-<main>
+<main class="rp-ArtworkArchive__main">
   <ul class="rp-ArtworkList rp-ArtworkList-grid">
   <?php while(have_posts()): the_post(); ?>
     <?php get_template_part('partials/artwork-inGallery'); ?>
