517d1a86c2d8c4b44b4ab89030b6eb7ac9d93c92
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-08 13:00:16 +0100

    Previous and next remain even if there is no previous or next post

--- www/wp-content/themes/romaricpascal.is/partials/prev-next.php
+++ www/wp-content/themes/romaricpascal.is/partials/prev-next.php
@@ -5,10 +5,15 @@ if(!empty($previous_post) || !empty($next_post)):
 ?>
 <nav class="rp-PrevNextNav">
 	<?php if(!empty($previous_post)): ?>
-	<a class="rp-PrevNextLink rp-PrevNextLink-previous rp-PrevNextNav__prev" href="<?= get_permalink($previous_post->ID) ?>" title="<?=$previous_post->post_title; ?>"><?php echo $previous_post->post_title; ?></a>
+	<a class="rp-PrevNextLink rp-PrevNextLink-previous rp-PrevNextNav__previous" href="<?= get_permalink($previous_post->ID) ?>" title="<?=$previous_post->post_title; ?>"><?php echo $previous_post->post_title; ?></a>
+	<?php else: ?>
+	<span class="rp-PrevNextLink rp-PrevNextLink-disabled rp-PrevNextLink-previous rp-PrevNextNav__previous"><?php echo $previous_post->post_title; ?></span>
 	<?php endif; ?>
 	<?php if(!empty($next_post)): ?>
-	<a class="rp-PrevNextLink rp-PrevNextLink-next rp-PrevNextNav__next" href="<?= get_permalink($next_post->ID) ?>" title="<?=$next_post->post_title; ?>"><?php echo $next_post->post_title; ?></a>
+	<a class="rp-PrevNextLink rp-PrevNextLink-next rp-PrevNextNav__next" href="<?= get_permalink($next_post->ID) ?>" title="<?=$next_post->post_title; ?>"
+	><?php echo $next_post->post_title; ?></a>
+	<?php else: ?>
+	<span class="rp-PrevNextLink rp-PrevNextLink-disabled rp-PrevNextLink-next rp-PrevNextNav__next"><?php echo $next_post->post_title; ?></span>
 	<?php endif; ?>	
 </nav>
 <?php endif; ?>
