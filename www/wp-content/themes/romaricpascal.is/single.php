c9de8224b4a0b80e3bc8cf0bd773441d0c3bfd3f
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-09 17:44:26 +0100

    Updated heading of blog articles

--- www/wp-content/themes/romaricpascal.is/single.php
+++ www/wp-content/themes/romaricpascal.is/single.php
@@ -1,31 +1,22 @@
 <?php the_post(); ?>
 <?php get_header(); ?>
 
-<section class="rp-LandingSection">
-  <div class="rp-Hero l-Container">
-    <h1 class="rp-HeroHeading rp-Underlined rp-Underlined-hero rp-Underlined-landing">
-      <?php the_title(); ?>
+<article class="rp-Content">
+  <header class="rp-HeaderWithSubhead">
+    <h1 class="rp-HeaderWithSubhead__heading">
+        <?php the_title(); ?>
     </h1>
-    <div>
-      <time datetime="<?php the_time('c'); ?>"><?php the_time('d M Y'); ?></time> —
-      <span>About: <?php the_terms(get_the_ID(), CRAFT_TAX_NAME);?>, <?php the_category(', '); ?></span>
-    </div>
-  </div>
-</section>
-
-<section class="rp-LandingSection t-light-on-dark">
-  <div class="l-Container">
+    <p><time datetime="<?php the_time('c'); ?>"><?php the_time('d M Y'); ?></time> — An article about: <?php the_terms(get_the_ID(), CRAFT_TAX_NAME);?> - <?php the_category(' - '); ?></p>
+  </header>
+  <main>
     <?php the_content(); ?>
-    <footer class="rp-ArtworkFullFooter rp-PreviousNextLinks">
-      <span class="rp-PreviousNextLinks__previous"><?php previous_post_link('%link', 'Older note'); ?></span>
-      <span class="rp-PreviousNextLinks__next"><?php next_post_link('%link', 'More recent note'); ?></span>
-    </footer>
-  </div>
-</section>
+  </main>
+  <aside>
 
-
+  </aside>
+  <?php get_template_part('partials/prev-next'); ?>
+</article>
 
 <?php
-  get_template_part('partials/c-stayupdated');
   get_footer();
 ?>
