6e70b3352ecc3280db214a23d4b4ab98a236e569
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-11 23:46:09 +0100

    Setup two columns layout for projects page

--- www/wp-content/themes/romaricpascal.is/single-project.php
+++ www/wp-content/themes/romaricpascal.is/single-project.php
@@ -1,21 +1,27 @@
 <?php the_post(); ?>
 <?php get_header(); ?>
 
-<article class="rp-Content">
-  <header class="rp-HeaderWithSubhead">
+<article class="rp-Single rp-Content">
+  <header class="rp-Single__header rp-HeaderWithSubhead">
     <h1 class="rp-HeaderWithSubhead__heading">
         <?php the_title(); ?>
     </h1>
     <?php the_post_thumbnail('full'); ?>
   </header>
-  <div>
+  <div class="rp-Single__main">
     <?php 
       the_content(); 
       get_template_part('single-project', 'testimonial');
       get_template_part('single-project', 'behind-the-scene');
     ?>
   </div>
-  <aside>
+  <aside class="rp-Single__aside">
+    <div class="rp-HeaderDuplicate rp-HeaderWithSubhead" role="presentation">
+      <h1 class="rp-HeaderWithSubhead__heading">
+        <?php the_title(); ?>
+      </h1>
+      <?php the_post_thumbnail('full'); ?>
+    </div>
     <?php 
       get_template_part('single-project','live-url');
       get_template_part('single-project', 'links');
