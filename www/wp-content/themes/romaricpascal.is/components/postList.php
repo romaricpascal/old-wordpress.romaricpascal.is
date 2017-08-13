a0905267085fb3f822a40f3ba6daaf5ed065ccae
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-13 17:40:18 +0100

    Added CSS classes so lists of posts display properly

--- www/wp-content/themes/romaricpascal.is/components/postList.php
+++ www/wp-content/themes/romaricpascal.is/components/postList.php
@@ -1,4 +1,8 @@
+<ul class="rp-ArchiveList <?php echo $classes; ?>">
 <?php while($query->have_posts()): $query->the_post(); ?>
 	<?php $post = rp_get_the_post(); ?>
-	<?php rp_render('post', ['post' => $post], [$format, $post->post_type]); ?>
-<?php endwhile; ?>
+	<li class="rp-ArchiveListItem rp-ArchiveListItem-<?php echo $format; ?> rp-ArchiveListItem-<?php echo $post->post_type;?>">
+		<?php rp_render('post', ['post' => $post], [$format, $post->post_type]); ?>
+	</li>
+<?php endwhile; wp_reset_postdata(); ?>
+</ul>
--- www/wp-content/themes/romaricpascal.is/partials/front-page/front-page-featured-archive.php
+++ www/wp-content/themes/romaricpascal.is/partials/front-page/front-page-featured-archive.php
@@ -1,7 +1,9 @@
-<?php $postListFormat = rp_get_postListFormat($postType->name); ?>
+<?php 
+	$postTypeName = $postType->name;
+	$postListFormat = rp_get_postListFormat($postTypeName); ?>
 <section class="rp-HomeSection">
 	<header>
 		<h2><?php echo $postType->label; ?></h2>
 	</header>
-	<?php rp_render('postList', ['query' => $query, 'format' => $postListFormat]); ?>
+	<?php rp_render('postList', ['query' => $query, 'classes' => "rp-ArchiveList-${postTypeName}", 'format' => $postListFormat]); ?>
 </section>
