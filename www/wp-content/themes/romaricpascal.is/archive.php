a73c372d8df2566a6a2e1291efbc72e0310f9d64
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-14 17:24:47 +0100

    Fixed duplicate display of posts in archives

--- www/wp-content/themes/romaricpascal.is/archive.php
+++ www/wp-content/themes/romaricpascal.is/archive.php
@@ -11,18 +11,7 @@
   </header>
 
 <div class="rp-Archive__main">
-  <?php rp_render('postList', ['classes' => "rp-ArchiveList-{$post_type}"]); ?>
-  <ul class="rp-ArchiveList rp-ArchiveList-<?php echo $post_type ?>">
-  <?php while(have_posts()): the_post(); ?>
-    <li class="rp-ArchiveListItem rp-ArchiveListItem-<?php echo $post_type ?> <?php if ($showThumbnailLink) {echo 'rp-ArchiveListItem-thumbnail';} ?>">
-      <?php if ($showThumbnailLink) {
-          get_template_part('partials/post-thumbnailLink', $post_type);
-        } else {
-          get_template_part('partials/post-link', $post_type);
-        } ?>
-    </li>
-  <?php endwhile; ?>
-  </ul>
+  <?php rp_render('postList', ['classes' => "rp-ArchiveList-{$post_type}", 'format' => rp_get_postListFormat($post_type)]); ?>
   <?php get_template_part('partials/prev-next-archive'); ?>
 </div>
 </div>
