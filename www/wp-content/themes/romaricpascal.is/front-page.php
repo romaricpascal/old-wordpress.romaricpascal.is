3710f89d8eb57085ec07c882721b0c013211ed92
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-22 10:37:10 +0100

    Started integration of lettering headings

--- www/wp-content/themes/romaricpascal.is/front-page.php
+++ www/wp-content/themes/romaricpascal.is/front-page.php
@@ -4,7 +4,22 @@
 ?>
 <?php get_header(); ?>
 <section class="rp-HomeSection">
-<?php the_content(); ?>
+<div class="rp-LetteredHeading rp-LetteredHeading-greeting">
+  <span class="rp-LetteredHeading__me rp-LetteredHeading__part">
+  </span>
+  <span class="rp-LetteredHeading__hi rp-LetteredHeading__part">Hi!
+  </span>
+  <span class="rp-LetteredHeading__im rp-LetteredHeading__part">I'm
+  </span>
+  <span class="rp-LetteredHeading__romaric rp-LetteredHeading__part">Romaric.
+  </span>
+  <span class="rp-LetteredHeading__romaricArrow rp-LetteredHeading__part"></span>
+  <span class="rp-LetteredHeading__draw rp-LetteredHeading__part">I draw
+  </span>
+  <span class="rp-LetteredHeading__letters rp-LetteredHeading__part">letters
+  </span>
+  <span class="rp-LetteredHeading__lettersArrow rp-LetteredHeading__part"></span>
+</div>
 </section>
 <?php get_template_part('partials/front-page/front-page','additional-sections'); ?>
 <?php get_footer(); ?>
