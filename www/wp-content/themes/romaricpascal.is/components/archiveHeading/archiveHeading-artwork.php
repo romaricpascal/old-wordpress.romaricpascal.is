013c25b15bb567ffa09b4b961be310143cf4b3c8
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-22 11:24:28 +0100

    Artwork archive heading & home page spacing

---
+++ www/wp-content/themes/romaricpascal.is/components/archiveHeading/archiveHeading-artwork.php
@@ -0,0 +1,7 @@
+<h<?php echo $headingLevel;?> class="rp-LetteredHeading rp-LetteredHeading-look">
+  <span class="rp-LetteredHeading__picture rp-LetteredHeading__part"></span>
+  <span class="rp-LetteredHeading__have rp-LetteredHeading__part">Have</span>
+  <span class="rp-LetteredHeading__a rp-LetteredHeading__part">a</span>
+  <span class="rp-LetteredHeading__look rp-LetteredHeading__part">look</span>
+  <span class="rp-LetteredHeading__etc rp-LetteredHeading__part">…</span>
+</h<?php echo $headingLevel;?>>
