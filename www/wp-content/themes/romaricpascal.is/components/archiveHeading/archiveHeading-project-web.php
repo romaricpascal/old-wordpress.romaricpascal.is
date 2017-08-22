377a071efd032b6a39b45c0db61fee5e3451fc41
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-22 12:09:28 +0100

    All headings are setup on the homepage

---
+++ www/wp-content/themes/romaricpascal.is/components/archiveHeading/archiveHeading-project-web.php
@@ -0,0 +1,6 @@
+<h<?php echo $headingLevel;?> class="rp-LetteredHeading rp-LetteredHeading-websites">
+  <span class="rp-LetteredHeading__picture rp-LetteredHeading__part"></span>
+  <span class="rp-LetteredHeading__iAlso rp-LetteredHeading__part">I also</span>
+  <span class="rp-LetteredHeading__make rp-LetteredHeading__part">make</span>
+  <span class="rp-LetteredHeading__website rp-LetteredHeading__part">websites.</span>  
+</h<?php echo $headingLevel;?>>
