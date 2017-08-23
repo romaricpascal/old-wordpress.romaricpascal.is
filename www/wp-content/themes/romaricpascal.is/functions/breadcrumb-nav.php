945bf89ea72001261b3adb57940faf9c2a238226
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-23 22:57:03 +0100

    Projects breadcrumbs show craft terms

--- www/wp-content/themes/romaricpascal.is/functions/breadcrumb-nav.php
+++ www/wp-content/themes/romaricpascal.is/functions/breadcrumb-nav.php
@@ -6,6 +6,15 @@
   		$labels = get_post_type_object($postType)->label;
   		$link = get_post_type_archive_link( $postType );
   		echo "<a class='rp-BreadcrumbLink' href='$link'>$labels</a>";
+
+  		if ($postType === PROJECT_TYPE) {
+
+  			$terms = rp_get_url_terms();
+  			foreach ($terms as $term) {
+  				$link .= "{$term->slug}/";
+  				echo "<a class='rp-BreadcrumbLink' href='{$link}'>$term->name</a>";
+  			}
+  		}
   	}
   }
 
