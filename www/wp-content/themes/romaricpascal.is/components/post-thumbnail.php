c63d880ff0921b85b7901cec7b023fdf53b56172
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-25 22:23:25 +0100

    Enforced 1:1 ratio on post-thumbnails so the layout doesn't break when loading new archive content

--- www/wp-content/themes/romaricpascal.is/components/post-thumbnail.php
+++ www/wp-content/themes/romaricpascal.is/components/post-thumbnail.php
@@ -1,3 +1,3 @@
-<a href="<?php echo rp_get_permalink($post, $craft); ?>">
-	<?php rp_render('postThumbnailImg', ['post' => $post, 'classes' => 'u-d-b']); ?>
+<a class="o-ratio" href="<?php echo rp_get_permalink($post, $craft); ?>">
+	<?php rp_render('postThumbnailImg', ['post' => $post, 'classes' => 'o-ratio__content u-d-b']); ?>
 </a>
