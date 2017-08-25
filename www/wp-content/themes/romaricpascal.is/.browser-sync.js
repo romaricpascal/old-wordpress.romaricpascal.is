38654c60a301463139abef80a3b513d1ba1e4eb0
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-25 18:13:13 +0100

    Made BrowserSync properly proxy response headers

--- www/wp-content/themes/romaricpascal.is/.browser-sync.js
+++ www/wp-content/themes/romaricpascal.is/.browser-sync.js
@@ -3,6 +3,12 @@ module.exports = {
 		target: 'localhost:8080',
 		proxyReq: [function (proxyReq, req, res, options) {
 			proxyReq.setHeader('Host', req.headers.host);
+		}],
+		proxyRes: [function (proxyRes, req, res) {
+			console.log(proxyRes.headers);
+			Object.keys(proxyRes.headers).forEach(function (headerName) {
+				res.setHeader(headerName, proxyRes.headers[headerName]);
+			})
 		}]
 	},
 	files: ['style.css', 'main.js', '**/*.php'],
