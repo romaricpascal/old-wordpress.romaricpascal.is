b0328c7633762845485522b53c4ccec07a230e4f
Romaric Pascal <romaric.pascal@gmail.com>
2017-08-29 16:09:54 +0100

    Scrolling through home page now triggers update of URL

--- www/wp-content/themes/romaricpascal.is/js/activeFragment.js
+++ www/wp-content/themes/romaricpascal.is/js/activeFragment.js
@@ -1,4 +1,21 @@
-(function () {
+import ScrollIntersectionObserver from './ScrollIntersectionObserver';
 
-	
-})();
+function highest(property) {
+	return function (current, value) {
+		if (!current || value[property] > current[property]) {
+			return value;
+		}
+
+		return current;
+	}
+}
+
+function updateMostVisible(entries) {
+	var mostVisible = entries.reduce(highest('intersectionRatio'));
+	console.log(entries, mostVisible);
+	history.pushState({}, document.title, `#${mostVisible.target.id}`);
+}
+
+var observer = new ScrollIntersectionObserver(updateMostVisible);
+observer.observe('[data-inview]');
+updateMostVisible(observer.takeRecords());
