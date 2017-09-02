15d825cd3017e6f7dcc64e6eb0a2574aebeeae4c
Romaric Pascal <romaric.pascal@gmail.com>
2017-09-02 16:58:04 +0100

    Removed partials that are no longer used

--- www/wp-content/themes/romaricpascal.is/partials/artwork-inGallery.php
+++
@@ -1,5 +0,0 @@
-<a href="<?php the_permalink(); ?>" class="rp-ArtworkListItem">
-	<img class="rp-ArtworkImage" src="<?php the_post_thumbnail_url('artwork-grid-xl'); ?>" title="<?php  the_title();?>"
-		alt="<?php the_title();?>"
-		srcset="<?php rp_the_thumbnail_srcset(['artwork-grid-s','artwork-grid-m','artwork-grid-l', 'artwork-grid-xl', 'artwork-grid-l-3x']); ?>">
-</a>
