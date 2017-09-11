94583e0276f0bf28b166ce5594f487cc13f61c31
Romaric Pascal <hello@romaricpascal.is>
2017-09-11 10:35:48 +0100

    Fixed content markup for testimonials

--- www/wp-content/themes/romaricpascal.is/components/post-full-testimonial.php
+++ www/wp-content/themes/romaricpascal.is/components/post-full-testimonial.php
@@ -1,4 +1,4 @@
 <figure class="<?= $classes; ?>">
-	<p><?= rp_get_content($post); ?></p>
+	<?= rp_get_content($post); ?>
 	<figcaption class="t-typo-display u-ta-c"><?php the_testimonial_author($post) ?><?php the_testimonial_link($post, ', ') ?></figcaption>
 </figure>
