09f931226e326998dfe9e673183a68bb63b4ea09
Romaric Pascal <hello@romaricpascal.is>
2017-09-11 08:45:34 +0100

    Fixed broken links when paginated in lettering archive description

--- www/wp-content/themes/romaricpascal.is/components/archiveDescription/archiveDescription-project-lettering.php
+++ www/wp-content/themes/romaricpascal.is/components/archiveDescription/archiveDescription-project-lettering.php
@@ -3,9 +3,9 @@
 
 	<p>This helps your message make the right impact to your audience:</p>
 	<ul>
-		<li>share what makes your project or business unique through its <a href="../logo-brand-designs">logo and brand</a></li>
-		<li>deliver memorable and impactful messages on <a href="../print-illustration-designs">prints and in illustrations</a></li>
-		<li>give <a href="../tshirts-merch-designs">t-shirts and other objects</a> their own personality</li>
+		<li>share what makes your project or business unique through its <a href="/proud-of/logo-brand-designs">logo and brand</a></li>
+		<li>deliver memorable and impactful messages on <a href="/proud-of/print-illustration-designs">prints and in illustrations</a></li>
+		<li>give <a href="/proud-of/tshirts-merch-designs">t-shirts and other objects</a> their own personality</li>
 	</ul>
 
 	<p>These are only a few examples. If you're looking to get some custom letters, feel free to <a href="mailto:<?= get_bloginfo('admin_email')?>">get in touch!</a></p>
