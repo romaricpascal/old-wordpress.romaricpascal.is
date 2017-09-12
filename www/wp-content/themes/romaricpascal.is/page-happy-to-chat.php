382ee373337a5e37c33415e294406aece95c0f86
Romaric Pascal <hello@romaricpascal.is>
2017-09-12 09:27:50 +0100

    Updated templates to match new contact page

--- www/wp-content/themes/romaricpascal.is/page-happy-to-chat.php
+++ www/wp-content/themes/romaricpascal.is/page-happy-to-chat.php
@@ -5,7 +5,7 @@ Template Name: Contact
 the_post();
 get_header(); ?>
 <div class="rp-HomeSection u-mw-30em-xl-down" data-inview>
-	<?php rp_render('page', ['post' => rp_get_the_post()], ['contact']);?>
+	<?php rp_render('page', ['post' => rp_get_the_post()], ['happy-to-chat']);?>
 </div>
 <?php 
 get_footer();
