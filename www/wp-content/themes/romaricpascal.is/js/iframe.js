22c93909adcfa513b7542552511ae4be798a7ef4
Romaric Pascal <hello@romaricpascal.is>
2017-09-21 19:58:26 +0100

    Fixed eslint warning

--- www/wp-content/themes/romaricpascal.is/js/iframe.js
+++ www/wp-content/themes/romaricpascal.is/js/iframe.js
@@ -1,23 +1,29 @@
 const FRAME_ORIENTATION_PORTRAIT = 'rp-OrientableFrame-portrait';
 
+function handleRotate(e) {
+	var targetSelector =e.target.getAttribute('data-target');
+	var target = document.querySelector(targetSelector);
+	if (target) {
+		target.classList.toggle(FRAME_ORIENTATION_PORTRAIT);
+	}
+}
+
+function handleReload(e) {
+	var targetSelector = e.target.getAttribute('data-target');
+	var target = document.querySelector(targetSelector);
+	if (target) {
+		target.contentWindow.location.reload(true)
+	}
+}
+
 if (document.body.classList) {
 	document.body.addEventListener('click', e => {
 		if (e.target.matches('.js-rotateIframe')) {
-			var targetSelector =e.target.getAttribute('data-target');
-			var target = document.querySelector(targetSelector);
-			if (target) {
-				target.classList.toggle(FRAME_ORIENTATION_PORTRAIT);
-			}
-			return;
+			return handleRotate(e);
 		}
 
 		if (e.target.matches('.js-reloadIframe')) {
-			var targetSelector = e.target.getAttribute('data-target');
-			var target = document.querySelector(targetSelector);
-			if (target) {
-				target.contentWindow.location.reload(true)
-			}
-			return;
+			return handleReload(e);
 		}
 	});
 }
