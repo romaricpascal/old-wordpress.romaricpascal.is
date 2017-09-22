f7ad322d30e0f4aa10458cd9f33b83f34fa87e34
Romaric Pascal <romaric.pascal@gmail.com>
2017-09-22 12:24:20 +0100

    Started implementation of scroll tied animations

--- www/wp-content/themes/romaricpascal.is/js/inview.js
+++ www/wp-content/themes/romaricpascal.is/js/inview.js
@@ -1,12 +1,13 @@
 import ScrollIntersectionObserver from './ScrollIntersectionObserver';
+import linear from 'd3-scale/src/linear';
+
+var opacity = linear()
+  .domain([0.15, 0.5])
+  .range([0, 1]);
 
 function markElementsInView(entries) {
 	entries.forEach(function (entry) {
-		if (entry.isIntersecting && entry.intersectionRatio > 0.25) {
-			entry.target.classList.add('is-inView');
-		} else {
-			entry.target.classList.remove('is-inView');
-		}
+		entry.target.style.opacity = opacity(entry.viewportIntersectionRatio);
 	});
 }
 
@@ -14,3 +15,22 @@ var observer = new ScrollIntersectionObserver(markElementsInView);
 
 observer.observe('[data-inview]');
 markElementsInView(observer.takeRecords());
+
+var translateX = linear()
+ .domain([0.15, 0.75])
+ .clamp(true)
+ .range([-100, 0]);
+
+function translatePosts(entries) {
+	entries.forEach(function (entry) {
+		var distanceFromBottom = entry.rootBounds.bottom - entry.boundingClientRect.top;
+		var percentDistanceFromBottom = distanceFromBottom / entry.boundingClientRect.height;
+		var translation = translateX(percentDistanceFromBottom);
+		entry.target.style.opacity = opacity(percentDistanceFromBottom);
+		entry.target.style.transform = `translateX(${translation}%)`;
+	});
+}
+
+var translateObserver = new ScrollIntersectionObserver(translatePosts);
+translateObserver.observe('.rp-PostLink');
+translatePosts(translateObserver.takeRecords());
