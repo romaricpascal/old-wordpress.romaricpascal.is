dab461ec847095082c85f18dc1f2a04bf5da0f76
Romaric Pascal <hello@romaricpascal.is>
2017-09-26 16:07:31 +0100

    Fixed pagination links on single posts and projects

--- www/wp-content/themes/romaricpascal.is/partials/prev-next.php
+++ www/wp-content/themes/romaricpascal.is/partials/prev-next.php
@@ -6,14 +6,14 @@ $next_post = rp_get_next_post($post, $terms);
 if(!empty($previous_post) || !empty($next_post)):
 ?>
 <nav class="rp-PrevNextNav">
-	<?php rp_render('prevNextLink', [
+	<?php rp_render('pagination/prevNextLink', [
 		'classes' => 'rp-PrevNextNav__previous',
 		'rel' => 'prev',
 		'href' => rp_get_permalink($previous_post, $terms),
 		'title' => rp_get($previous_post, 'post_title'),
 		'empty' => 'No previous post'
 	]); ?>
-	<?php rp_render('prevNextLink', [
+	<?php rp_render('pagination/prevNextLink', [
 		'classes' => 'rp-PrevNextNav__next',
 		'rel' => 'next',
 		'href' => rp_get_permalink($next_post, $terms),
