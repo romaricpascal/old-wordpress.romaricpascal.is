205354c2a27ad09f26047eb108acc39c1ea8e7ac
Romaric Pascal <hello@romaricpascal.is>
2017-10-13 17:57:40 +0100

    Added fallback for snakeGrid when grid layout is not supported

--- www/wp-content/themes/romaricpascal.is/components/postList-artwork.php
+++ www/wp-content/themes/romaricpascal.is/components/postList-artwork.php
@@ -3,14 +3,14 @@
 		if ($index % 12 === 0 || $index % 12 - 7 === 0) {
 			return '400';
 		} else {
-			return '200';
+			return '400';
 		}
 	};
 	$props['sizes'] = function ($index) {
 		if ($index % 12 === 0 || $index % 12 - 7 === 0) {
-			return '(min-width:20em) 400px ,200px';
+			return '(screen and (-ms-high-contrast: active), (-ms-high-contrast: none)) 400px,  (min-width:20em) 400px, 200px';
 		} else {
-			return '200px';
+			return '(screen and (-ms-high-contrast: active), (-ms-high-contrast: none)) 400px, 200px';
 		}
 	};
 	rp_render('postList', $props);
